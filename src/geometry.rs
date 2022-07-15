6d33a3af9d4c8da9b40fc399ac54f28e5cd75649
Kirill Chibisov <contact@kchibisov.com>
2022-07-15 17:51:55 +0300

    Apply mount_matrix on accelerometer results
    Orientation handling was not applying mount_matrix leading to
    broken rotation on devices that don't have an identity matrix
    as their acceleration matrix.

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,8 +1,113 @@
 //! Extension's to Smithay's geometry module.
 
-use std::cmp;
+use std::str::FromStr;
+use std::{cmp, error, fmt, ops};
 
 use smithay::utils::{Point, Size};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Matrix3x3<T: Copy> {
+    storage: Vec<T>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct FromVecError(usize, usize);
+
+impl fmt::Display for FromVecError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Mismatched size when createing object from vector, expected {} got {}",
+            self.0, self.1
+        )
+    }
+}
+
+impl error::Error for FromVecError {}
+
+impl<T: Copy> TryFrom<Vec<T>> for Matrix3x3<T> {
+    type Error = Box<dyn error::Error>;
+
+    fn try_from(storage: Vec<T>) -> Result<Self, Self::Error> {
+        if storage.len() != 9 {
+            Err(Box::new(FromVecError(9, storage.len())))
+        } else {
+            Ok(Self { storage })
+        }
+    }
+}
+
+impl ops::Mul<&Matrix3x3<f32>> for Vector3D<f32> {
+    type Output = Vector3D<f32>;
+
+    fn mul(self, rhs: &Matrix3x3<f32>) -> Self::Output {
+        let x = rhs.storage[0] * self.x + rhs.storage[3] * self.y + rhs.storage[6] * self.z;
+        let y = rhs.storage[1] * self.x + rhs.storage[4] * self.y + rhs.storage[7] * self.z;
+        let z = rhs.storage[2] * self.x + rhs.storage[5] * self.y + rhs.storage[8] * self.z;
+        Self::Output { x, y, z }
+    }
+}
+
+// The accepted formats is is 0, 0, 0; 0, 0, 0; 0, 0, 0.
+impl<T: FromStr + Copy> FromStr for Matrix3x3<T>
+where
+    <T as FromStr>::Err: std::error::Error + 'static,
+{
+    type Err = Box<dyn error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rows = s
+            .split(';')
+            // Ensure that we process only 3 rows.
+            .take(3)
+            // Take only 3 element from each row.
+            .flat_map(|row| row.split(',').take(3))
+            .map(|ch| T::from_str(ch.trim()))
+            .collect::<Result<Vec<T>, _>>()?;
+
+        Self::try_from(rows)
+    }
+}
+
+/// A point in the 3 dimensional space.
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Vector3D<T: Copy> {
+    pub x: T,
+    pub y: T,
+    pub z: T,
+}
+
+impl<T: Copy> Vector3D<T> {
+    #[inline]
+    pub fn new(x: T, y: T, z: T) -> Self {
+        Self { x, y, z }
+    }
+}
+
+impl Vector for Vector3D<f32> {
+    fn scale(&self, scale: f64) -> Self {
+        let x = scale as f32 * self.x;
+        let y = scale as f32 * self.y;
+        let z = scale as f32 * self.z;
+        Self { x, y, z }
+    }
+
+    fn min(&self, other: impl Into<Self>) -> Self {
+        let other = other.into();
+        let x = self.x.min(other.x);
+        let y = self.y.min(other.y);
+        let z = self.z.min(other.z);
+        Self { x, y, z }
+    }
+
+    fn max(&self, other: impl Into<Self>) -> Self {
+        let other = other.into();
+        let x = self.x.max(other.x);
+        let y = self.y.max(other.y);
+        let z = self.z.max(other.z);
+        Self { x, y, z }
+    }
+}
 
 pub trait Vector: Sized {
     /// Scale the size by a scaling factor.
@@ -55,5 +160,39 @@ where
         let tuple = self.as_vector();
         let other = other.into().as_vector();
         Self::from((cmp::max(tuple.0, other.0), cmp::max(tuple.1, other.1)))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_matrix() {
+        let matrix = "1, 0, 0; 0, 1, 0; 0, 0, 1";
+        assert_eq!(
+            vec![1, 0, 0, 0, 1, 0, 0, 0, 1],
+            Matrix3x3::<i32>::from_str(matrix).unwrap().storage
+        );
+
+        let matrix = "1, 0, 0, 0, 1, 0, 0, 0, 1";
+        assert_eq!(
+            &FromVecError(9, 3),
+            Matrix3x3::<i32>::from_str(matrix)
+                .err()
+                .unwrap()
+                .downcast_ref::<FromVecError>()
+                .unwrap()
+        );
+
+        let matrix = "1; 0; 0; 0; 1; 0; 0; 0; 1;";
+        assert_eq!(
+            &FromVecError(9, 3),
+            Matrix3x3::<i32>::from_str(matrix)
+                .err()
+                .unwrap()
+                .downcast_ref::<FromVecError>()
+                .unwrap()
+        );
     }
 }
