2087eb4f09849468e0e053120a40583c4c4ae055
Christian Duerr <contact@christianduerr.com>
2022-07-31 00:48:37 +0200

    Remove daemonization from daemon::spawn
    The `daemon::spawn` function was causing a runtime crash whenever it was
    invoked with `pre_exec` present to run the `post_start.sh` script. This
    patch temporarily removes the daemonization to avoid this crash. This
    will cause zombies to be spawned but at least the compositor doesn't
    crash.

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,6 +1,5 @@
 use std::ffi::OsStr;
 use std::io;
-use std::os::unix::process::CommandExt;
 use std::process::{Command, Stdio};
 
 pub fn spawn<I, S>(program: S, args: I) -> io::Result<()>
@@ -8,25 +7,12 @@ where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
 {
-    let mut command = Command::new(program);
-    command.args(args).stdin(Stdio::null()).stdout(Stdio::null()).stderr(Stdio::null());
-    unsafe {
-        command
-            .pre_exec(|| {
-                match libc::fork() {
-                    -1 => return Err(io::Error::last_os_error()),
-                    0 => (),
-                    _ => libc::_exit(0),
-                }
+    Command::new(program)
+        .args(args)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()?;
 
-                if libc::setsid() == -1 {
-                    return Err(io::Error::last_os_error());
-                }
-
-                Ok(())
-            })
-            .spawn()?
-            .wait()
-            .map(|_| ())
-    }
+    Ok(())
 }
