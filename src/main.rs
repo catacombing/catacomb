4b19348901901e33e27be496c533f1ab1d421da9
Christian Duerr <contact@christianduerr.com>
2022-08-18 06:00:29 +0200

    Remove SIG_IGN SIGCHLD handler
    The SIG_IGN SIGCHLD handler can cause issues when parent processes rely
    on SIGCHLD events to be handled, which is the case for applications like
    Epitaph which try to shell out operations to different programs.
    
    To still be able to spawn the user startup script without having a
    zombie hang around, the previous version of the `daemon` module has been
    reintroduced.
    
    Closes #7.

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ mod window;
 mod winit;
 
 fn main() {
-    // Do not turn children into zombies.
-    unsafe { libc::signal(libc::SIGCHLD, libc::SIG_IGN) };
-
     if env::var_os("DISPLAY").is_none() && env::var_os("WAYLAND_DISPLAY").is_none() {
         udev::run();
     } else {
