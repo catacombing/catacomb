catacombing/catacomb

commit f1be5e4b7497ef46f4ce2cc54f9f6cc745450e4d

Fix popup render window clamping
Previously all windows, including popups, would be clamped to their
specified window geometry. Since many windows, especially popups, render
shadows outside of their geometry, this causes surfaces to look "chopped
off".

Instead of cutting off popups, the popups geometry is now unrestricted,
making it possible for popups to render even above other windows.

This comes with a bit of a change to the rendering functions, which now
take a location plus a bounds rectangle, instead of just taking a bounds
rectangle. This is necessary since otherwise the top-left corner would
get clipped with a positive geometry location.

Closes #66.

src/drawing.rs
@@ -105,33 +105,34 @@ impl Texture {
 
     /// Render the texture at the specified location.
     ///
-    /// Using the `window_bounds` and `window_scale` parameters, it is possible
-    /// to scale the texture and truncate it to be within the specified
-    /// window bounds. The scaling will always take part **before** the
-    /// truncation.
+    /// Bounds is an absolute rectangle to which the window's rendering will be
+    /// clipped. Passing `None` will not clamp the window.
     pub fn draw_at(
         &self,
         frame: &mut Gles2Frame,
         canvas: &Canvas,
-        window_bounds: Rectangle<i32, Logical>,
-        window_scale: f64,
+        location: Point<i32, Logical>,
+        bounds: impl Into<Option<Rectangle<i32, Logical>>>,
+        scale: f64,
         damage: impl Into<Option<Rectangle<i32, Physical>>>,
     ) {
-        // Skip textures completely outside of the window bounds.
-        let scaled_window_bounds = window_bounds.size.scale(1. / window_scale).max((1, 1));
-        if self.location.x >= scaled_window_bounds.w || self.location.y >= scaled_window_bounds.h {
-            return;
-        }
+        // Clamp source rectangle to render bounds.
+        let mut src = Rectangle::from_loc_and_size((0, 0), self.size);
+        if let Some(mut bounds) = bounds.into() {
+            // Convert bounds to be relative to window origin.
+            bounds.loc += self.location - location;
 
-        // Truncate source size based on window bounds.
-        let src_size = (self.size.to_point() + self.location).to_size().min(scaled_window_bounds);
-        let src = Rectangle::from_loc_and_size((0, 0), src_size);
+            src = match bounds.intersection(src) {
+                Some(src) => src,
+                // Skip rendering if texture is completely outside of bounds.
+                None => return,
+            };
+        }
         let src_buffer = src.to_buffer(self.scale, self.transform, &self.size);
 
         // Scale output size based on window scale.
-        let location = window_bounds.loc + self.location.scale(window_scale);
-        let dst_size = src_size.scale(window_scale).min(window_bounds.size);
-        let dst = Rectangle::from_loc_and_size(location, dst_size);
+        let dst_location = location + self.location.scale(scale);
+        let dst = Rectangle::from_loc_and_size(dst_location, src.size.scale(scale));
         let dst_physical = dst.to_physical(canvas.scale());
 
         // Calculate surface damage.

src/overview.rs
@@ -213,7 +213,9 @@ impl Overview {
                 let mut bounds = position.bounds;
                 self.handle_closing(output, canvas, layouts, primary, &mut bounds, true);
 
-                primary.borrow_mut().draw(frame, canvas, position.scale, bounds, None, None);
+                let scale = position.scale;
+                let loc = bounds.loc;
+                primary.borrow_mut().draw(frame, canvas, scale, loc, None, None, None);
             }
 
             // Draw the secondary window.
@@ -222,7 +224,9 @@ impl Overview {
                 let mut bounds = position.secondary_bounds();
                 self.handle_closing(output, canvas, layouts, secondary, &mut bounds, false);
 
-                secondary.borrow_mut().draw(frame, canvas, position.scale, bounds, None, None);
+                let scale = position.scale;
+                let loc = bounds.loc;
+                secondary.borrow_mut().draw(frame, canvas, scale, loc, None, None, None);
             }
 
             offset += 1.;
@@ -289,7 +293,7 @@ pub struct DragAndDrop {
     pub touch_position: Point<f64, Logical>,
     pub window: Rc<RefCell<Window>>,
     pub overview_x_offset: f64,
-    window_bounds: Rectangle<i32, Logical>,
+    start_location: Point<i32, Logical>,
     scale: f64,
 }
 
@@ -308,12 +312,15 @@ impl DragAndDrop {
         let available = canvas.available_overview();
         let position = OverviewPosition::new(available, overview.x_offset, window_x_offset);
 
-        // Calculate original bounds of dragged window.
-        let window_bounds =
-            if layout_position.secondary { position.secondary_bounds() } else { position.bounds };
+        // Calculate original position of dragged window.
+        let start_location = if layout_position.secondary {
+            position.secondary_bounds().loc
+        } else {
+            position.bounds.loc
+        };
 
         Self {
-            window_bounds,
+            start_location,
             window,
             touch_position: overview.last_drag_point,
             overview_x_offset: overview.x_offset,
@@ -325,12 +332,11 @@ impl DragAndDrop {
     /// Draw the tiling location picker.
     pub fn draw(&self, frame: &mut Gles2Frame, canvas: &Canvas, graphics: &Graphics) {
         // Offset by dragged distance.
-        let mut bounds = self.window_bounds;
-        bounds.loc += self.window_position.to_i32_round();
+        let location = self.start_location + self.window_position.to_i32_round();
 
         // Render the window being drag-and-dropped.
         let mut window = self.window.borrow_mut();
-        window.draw(frame, canvas, self.scale, bounds, None, None);
+        window.draw(frame, canvas, self.scale, location, None, None, None);
 
         // Set custom OpenGL blending function.
         let _ = frame.with_context(|gl| unsafe {
@@ -345,9 +351,9 @@ impl DragAndDrop {
         let scale = cmp::max(available.size.w, available.size.h) as f64;
         for bounds in [primary_bounds, secondary_bounds] {
             if bounds.to_f64().contains(self.touch_position) {
-                graphics.active_drop_target.draw_at(frame, canvas, bounds, scale, None);
+                graphics.active_drop_target.draw_at(frame, canvas, bounds.loc, bounds, scale, None);
             } else {
-                graphics.drop_target.draw_at(frame, canvas, bounds, scale, None);
+                graphics.drop_target.draw_at(frame, canvas, bounds.loc, bounds, scale, None);
             }
         }
 

src/windows/mod.rs
@@ -305,7 +305,15 @@ impl Windows {
                 self.layers.draw_background(frame, &self.canvas, damage, &mut self.opaque_regions);
 
                 self.layouts.with_visible_mut(|window| {
-                    window.draw(frame, &self.canvas, 1., None, damage, &mut self.opaque_regions);
+                    window.draw(
+                        frame,
+                        &self.canvas,
+                        1.,
+                        None,
+                        None,
+                        damage,
+                        &mut self.opaque_regions,
+                    );
                 });
 
                 // Draw top/overlay windows in workspace view.
@@ -315,7 +323,7 @@ impl Windows {
                 self.layers.draw_background(frame, &self.canvas, damage, &mut self.opaque_regions);
 
                 self.layouts.with_visible_mut(|window| {
-                    window.draw(frame, &self.canvas, 1., None, damage, None);
+                    window.draw(frame, &self.canvas, 1., None, None, damage, None);
                 });
                 dnd.draw(frame, &self.canvas, graphics);
             },
@@ -330,7 +338,7 @@ impl Windows {
                 }
             },
             View::Fullscreen(window) => {
-                window.borrow_mut().draw(frame, &self.canvas, 1., None, None, None);
+                window.borrow_mut().draw(frame, &self.canvas, 1., None, None, None, None);
 
                 // Draw overlay windows in fullscreen view.
                 self.layers.draw_overlay(frame, &self.canvas, damage, &mut self.opaque_regions);

src/windows/window.rs
@@ -113,18 +113,33 @@ impl<S: Surface> Window<S> {
 
     /// Render this window's buffers.
     ///
-    /// If no location is specified, the textures cached location will be used.
+    /// If no location is specified, the texture's cached location will be used.
+    ///
+    /// Bounds is an absolute rectangle to which the window's rendering will be
+    /// clipped. Passing `None` will clamp the window to its geometry, cutting
+    /// off parts of the texture outside of it (i.e. shadows).
+    #[allow(clippy::too_many_arguments)]
     pub fn draw<'a>(
         &mut self,
         frame: &mut Gles2Frame,
         canvas: &Canvas,
         scale: f64,
+        location: impl Into<Option<Point<i32, Logical>>>,
         bounds: impl Into<Option<Rectangle<i32, Logical>>>,
         damage: impl Into<Option<&'a [Rectangle<i32, Physical>]>>,
         opaque_regions: impl Into<Option<&'a mut OpaqueRegions>>,
     ) {
-        let bounds = bounds.into().unwrap_or_else(|| self.bounds());
+        // Calculate render position and bounds.
+        let location = location.into();
+        let bounds = bounds.into().unwrap_or_else(|| {
+            let mut bounds = self.bounds();
+            if let Some(location) = location {
+                bounds.loc = location;
+            }
+            bounds
+        });
         let physical_bounds = bounds.to_physical(canvas.scale());
+        let location = location.unwrap_or(bounds.loc);
 
         // Treat no damage information as full damage.
         let full_damage = [physical_bounds];
@@ -163,14 +178,27 @@ impl<S: Surface> Window<S> {
         };
 
         for texture in &mut self.texture_cache.textures {
-            texture.draw_at(frame, canvas, bounds, scale, window_damage);
+            texture.draw_at(frame, canvas, location, bounds, scale, window_damage);
         }
 
         // Draw popup tree.
         for popup in &mut self.popups {
             let loc = bounds.loc + popup.rectangle.loc;
-            let popup_bounds = Rectangle::from_loc_and_size(loc, (i32::MAX, i32::MAX));
-            popup.draw(frame, canvas, scale, popup_bounds, damage, opaque_regions.as_deref_mut());
+
+            // Do not clamp popup bounds, to allow rendering even without the client
+            // geometry.
+            let popup_loc = loc - popup.surface.geometry().loc;
+            let popup_bounds = Rectangle::from_loc_and_size(popup_loc, (i32::MAX, i32::MAX));
+
+            popup.draw(
+                frame,
+                canvas,
+                scale,
+                loc,
+                popup_bounds,
+                damage,
+                opaque_regions.as_deref_mut(),
+            );
         }
     }
 

src/layer.rs
@@ -77,11 +77,11 @@ impl Layers {
         opaque_regions: &mut OpaqueRegions,
     ) {
         for window in &mut self.background {
-            window.draw(frame, canvas, 1., None, damage, &mut *opaque_regions);
+            window.draw(frame, canvas, 1., None, None, damage, &mut *opaque_regions);
         }
 
         for window in &mut self.bottom {
-            window.draw(frame, canvas, 1., None, damage, &mut *opaque_regions);
+            window.draw(frame, canvas, 1., None, None, damage, &mut *opaque_regions);
         }
     }
 
@@ -94,7 +94,7 @@ impl Layers {
         opaque_regions: &mut OpaqueRegions,
     ) {
         for window in &mut self.top {
-            window.draw(frame, canvas, 1., None, damage, &mut *opaque_regions);
+            window.draw(frame, canvas, 1., None, None, damage, &mut *opaque_regions);
         }
 
         self.draw_overlay(frame, canvas, damage, opaque_regions);
@@ -109,7 +109,7 @@ impl Layers {
         opaque_regions: &mut OpaqueRegions,
     ) {
         for window in &mut self.overlay {
-            window.draw(frame, canvas, 1., None, damage, &mut *opaque_regions);
+            window.draw(frame, canvas, 1., None, None, damage, &mut *opaque_regions);
         }
     }
 

commit 3bf8c7d4e5bdfb34c8ce861f90674540b9e9f476

Fix scaled window clamping
This resolves an issue introduced in f1be5e4 where the D&D texture
bounds were not clamped to the correct size since only the source
rectangle was getting clamped.

Now the destination size is also clamped during rendering, which will
automatically resize windows which are still bigger than their target
rectangle even after texture clamping.

Since a clamped destination rectangle causes downscaling rather than
clamping, this is only suitable for textures like the popup bounds which
have a flat color. However it should only ever have an effect on 1x1
textures anyway.

src/drawing.rs
@@ -112,27 +112,34 @@ impl Texture {
         frame: &mut Gles2Frame,
         canvas: &Canvas,
         location: Point<i32, Logical>,
-        bounds: impl Into<Option<Rectangle<i32, Logical>>>,
+        bounds: Rectangle<i32, Logical>,
         scale: f64,
         damage: impl Into<Option<Rectangle<i32, Physical>>>,
     ) {
-        // Clamp source rectangle to render bounds.
-        let mut src = Rectangle::from_loc_and_size((0, 0), self.size);
-        if let Some(mut bounds) = bounds.into() {
-            // Convert bounds to be relative to window origin.
-            bounds.loc += self.location - location;
+        // Convert destination bounds to texture-relative rectangle.
+        let mut scaled_bounds = bounds;
+        scaled_bounds.loc -= location;
+        scaled_bounds.size = scaled_bounds.size.scale(1. / scale).max((1, 1));
+        scaled_bounds.loc = scaled_bounds.loc.scale(1. / scale);
+        scaled_bounds.loc -= self.location;
 
-            src = match bounds.intersection(src) {
-                Some(src) => src,
-                // Skip rendering if texture is completely outside of bounds.
-                None => return,
-            };
-        }
+        // Calculate source texture rectangle.
+        let mut src = Rectangle::from_loc_and_size((0, 0), self.size);
+        src = match src.intersection(scaled_bounds) {
+            Some(src) => src,
+            // Skip rendering when completely outside of bounds.
+            None => return,
+        };
         let src_buffer = src.to_buffer(self.scale, self.transform, &self.size);
 
-        // Scale output size based on window scale.
-        let dst_location = location + self.location.scale(scale);
-        let dst = Rectangle::from_loc_and_size(dst_location, src.size.scale(scale));
+        // Calculate destination rectangle.
+        let dst_location = location + (src.loc + self.location).scale(scale);
+        let mut dst = Rectangle::from_loc_and_size(dst_location, src.size.scale(scale));
+        dst = match dst.intersection(bounds) {
+            Some(dst) => dst,
+            // Skip rendering when completely outside of bounds.
+            None => return,
+        };
         let dst_physical = dst.to_physical(canvas.scale());
 
         // Calculate surface damage.

src/input.rs
@@ -472,10 +472,7 @@ impl Catacomb {
                 },
                 // Filter power buttons pressed during suspend.
                 (keysyms::KEY_XF86PowerOff, _)
-                    if catacomb.last_resume.elapsed() <= RESUME_INHIBIT_DURATION =>
-                {
-                    ()
-                },
+                    if catacomb.last_resume.elapsed() <= RESUME_INHIBIT_DURATION => {},
                 (keysyms::KEY_XF86PowerOff, KeyState::Pressed) => {
                     let id = catacomb.button_state.next_id();
                     catacomb.button_state.power = Some(id);

src/overview.rs
@@ -215,7 +215,7 @@ impl Overview {
 
                 let scale = position.scale;
                 let loc = bounds.loc;
-                primary.borrow_mut().draw(frame, canvas, scale, loc, None, None, None);
+                primary.borrow_mut().draw(frame, canvas, scale, loc, bounds, None, None);
             }
 
             // Draw the secondary window.
@@ -226,7 +226,7 @@ impl Overview {
 
                 let scale = position.scale;
                 let loc = bounds.loc;
-                secondary.borrow_mut().draw(frame, canvas, scale, loc, None, None, None);
+                secondary.borrow_mut().draw(frame, canvas, scale, loc, bounds, None, None);
             }
 
             offset += 1.;

src/windows/mod.rs
@@ -426,7 +426,7 @@ impl Windows {
         for rect in debug_damage {
             let bounds = rect.to_logical(self.canvas.scale());
             let scale = cmp::max(bounds.size.w, bounds.size.h) as f64;
-            graphics.damage_debug.draw_at(frame, &self.canvas, bounds, scale, None);
+            graphics.damage_debug.draw_at(frame, &self.canvas, bounds.loc, bounds, scale, None);
         }
 
         let _ = frame.with_context(|gl| unsafe {

src/windows/window.rs
@@ -132,10 +132,15 @@ impl<S: Surface> Window<S> {
         // Calculate render position and bounds.
         let location = location.into();
         let bounds = bounds.into().unwrap_or_else(|| {
+            // Get scaled window bounds.
             let mut bounds = self.bounds();
+            bounds.size = bounds.size.scale(scale);
+
+            // Override window location.
             if let Some(location) = location {
                 bounds.loc = location;
             }
+
             bounds
         });
         let physical_bounds = bounds.to_physical(canvas.scale());
@@ -183,11 +188,12 @@ impl<S: Surface> Window<S> {
 
         // Draw popup tree.
         for popup in &mut self.popups {
-            let loc = bounds.loc + popup.rectangle.loc;
+            // Calculate the popup's origin.
+            let loc = bounds.loc + popup.rectangle.loc.scale(scale);
 
             // Do not clamp popup bounds, to allow rendering even without the client
             // geometry.
-            let popup_loc = loc - popup.surface.geometry().loc;
+            let popup_loc = loc - popup.surface.geometry().loc.scale(scale);
             let popup_bounds = Rectangle::from_loc_and_size(popup_loc, (i32::MAX, i32::MAX));
 
             popup.draw(