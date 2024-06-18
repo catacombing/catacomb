catacombing/catacomb

commit 4a15dd274d098baef4be6fe4d6d2616c402c3a47

Simplify logging setup

src/main.rs
@@ -56,19 +56,10 @@ pub fn main() {
     // Try to initialize log-compatibility shim.
     let _ = LogTracer::init();
 
-    // Set default level to WARN with Catacomb itself at INFO.
-    let mut directives = String::from("warn,catacomb=info");
-
-    // Override with `RUST_LOG` env variable.
-    if let Ok(env_directives) = env::var("RUST_LOG") {
-        directives = env_directives;
-    }
-
-    // Setup tracing.
+    // Setup logging.
+    let directives = env::var("RUST_LOG").unwrap_or("warn,catacomb=info".into());
     let env_filter = EnvFilter::builder().parse_lossy(directives);
-    let subscriber =
-        FmtSubscriber::builder().with_env_filter(env_filter).with_line_number(true).finish();
-    let _ = tracing::subscriber::set_global_default(subscriber);
+    FmtSubscriber::builder().with_env_filter(env_filter).with_line_number(true).init();
 
     match Options::parse().subcommands {
         Some(Subcommands::Msg(msg)) => match catacomb_ipc::send_message(&msg) {

commit 0b0d4051ad70478bbfa64b0646d04f88f54262a0

Remove unnecessary logging init
This properly fixes the crash introduced in 4a15dd2 and fixed in
fb5dbaf, by removing the unnecessary `LogTracer::init` call which set
the default logger causing `tracing_subscriber` to crash due to the
conflict.

Cargo.toml
@@ -34,11 +34,10 @@ catacomb_ipc = { version = "0.1.0", path = "./catacomb_ipc", features = ["clap",
 clap = { version = "4.2.3", features = ["derive", "wrap_help"] }
 dirs = "5.0.0"
 libc = "0.2.123"
+profiling = { version = "1.0.14", optional = true, features = ["profile-with-puffin"] }
+puffin_http = { version = "0.16.0", optional = true }
 serde_json = "1.0.85"
 serde = { version = "1.0.144", features = ["derive"] }
 tracing = "0.1.37"
 tracing-subscriber = { version = "0.3.17", features = ["env-filter"] }
 udev = "0.8.0"
-tracing-log = "0.2.0"
-puffin_http = { version = "0.16.0", optional = true }
-profiling = { version = "1.0.14", optional = true, features = ["profile-with-puffin"] }

src/main.rs
@@ -12,7 +12,6 @@ use profiling::puffin;
 #[cfg(feature = "profiling")]
 use puffin_http::Server;
 use tracing::error;
-use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 mod catacomb;
@@ -53,15 +52,10 @@ pub fn main() {
         Server::new(&format!("0.0.0.0:{}", puffin_http::DEFAULT_PORT)).unwrap()
     };
 
-    // Try to initialize log-compatibility shim.
-    let _ = LogTracer::init();
-
     // Setup logging.
     let directives = env::var("RUST_LOG").unwrap_or("warn,catacomb=info".into());
     let env_filter = EnvFilter::builder().parse_lossy(directives);
-    let subscriber =
-        FmtSubscriber::builder().with_env_filter(env_filter).with_line_number(true).finish();
-    let _ = tracing::subscriber::set_global_default(subscriber);
+    FmtSubscriber::builder().with_env_filter(env_filter).with_line_number(true).init();
 
     match Options::parse().subcommands {
         Some(Subcommands::Msg(msg)) => match catacomb_ipc::send_message(&msg) {