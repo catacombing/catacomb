catacombing/catacomb

commit 9e88ce300b5766316b283ff9c91d4a3ffb72dfc6

Update to latest Smithay commit
This bumps the Smithay commit to the latest git version available and
deals with all breaking changes.

Cargo.toml
@@ -25,7 +25,7 @@ xkbcommon = "0.9.0"
 
 [workspace.dependencies.smithay]
 git = "https://github.com/smithay/smithay"
-rev = "06c9930f2b61f7d4b08a1f525fa114fd4492ac98"
+rev = "0cd72d182a7f20daa13d48a14752c95130f0bef9"
 default-features = false
 
 [dependencies]

src/catacomb.rs
@@ -13,8 +13,10 @@ use _server_decoration::server::org_kde_kwin_server_decoration_manager::Mode as 
 use calloop::Mode;
 use catacomb_ipc::{Keysym, Orientation};
 use smithay::backend::allocator::dmabuf::Dmabuf;
+use smithay::backend::input::KeyState;
 use smithay::backend::renderer::ImportDma;
-use smithay::input::keyboard::XkbConfig;
+use smithay::input::keyboard::xkb::ModMask;
+use smithay::input::keyboard::{KeyboardHandle, Keycode, XkbConfig};
 use smithay::input::{Seat, SeatHandler, SeatState};
 use smithay::reexports::calloop::generic::{Generic, NoIoDrop};
 use smithay::reexports::calloop::signals::{Signal, Signals};
@@ -50,7 +52,7 @@ use smithay::wayland::output::{OutputHandler, OutputManagerState};
 use smithay::wayland::presentation::PresentationState;
 use smithay::wayland::selection::SelectionHandler;
 use smithay::wayland::selection::data_device::{
-    self, ClientDndGrabHandler, DataDeviceHandler, DataDeviceState, ServerDndGrabHandler,
+    self, DataDeviceHandler, DataDeviceState, WaylandDndGrabHandler,
 };
 use smithay::wayland::selection::primary_selection::{
     self, PrimarySelectionHandler, PrimarySelectionState,
@@ -72,7 +74,7 @@ use smithay::wayland::single_pixel_buffer::SinglePixelBufferState;
 use smithay::wayland::socket::ListeningSocketSource;
 use smithay::wayland::text_input::TextInputManagerState;
 use smithay::wayland::viewporter::ViewporterState;
-use smithay::wayland::virtual_keyboard::VirtualKeyboardManagerState;
+use smithay::wayland::virtual_keyboard::{VirtualKeyboardHandler, VirtualKeyboardManagerState};
 use smithay::wayland::xdg_activation::{
     XdgActivationHandler, XdgActivationState, XdgActivationToken, XdgActivationTokenData,
 };
@@ -755,7 +757,28 @@ impl InputMethodHandler for Catacomb {
     }
 }
 
+// TODO
+impl VirtualKeyboardHandler for Catacomb {
+    fn on_keyboard_event(
+        &mut self,
+        _keycode: Keycode,
+        _state: KeyState,
+        _time: u32,
+        _keyboard: KeyboardHandle<Self>,
+    ) {
+    }
+
+    fn on_keyboard_modifiers(
+        &mut self,
+        _depressed_mods: ModMask,
+        _latched_mods: ModMask,
+        _locked_mods: ModMask,
+        _keyboard: KeyboardHandle<Self>,
+    ) {
+    }
+}
 delegate_virtual_keyboard_manager!(Catacomb);
+
 delegate_input_method_manager!(Catacomb);
 delegate_text_input_manager!(Catacomb);
 
@@ -893,8 +916,7 @@ impl DataDeviceHandler for Catacomb {
         &mut self.data_device_state
     }
 }
-impl ClientDndGrabHandler for Catacomb {}
-impl ServerDndGrabHandler for Catacomb {}
+impl WaylandDndGrabHandler for Catacomb {} // TODO?
 delegate_data_device!(Catacomb);
 
 impl DataControlHandler for Catacomb {

src/windows/surface.rs
@@ -99,7 +99,7 @@ impl Surface for ToplevelSurface {
                 .get::<Mutex<XdgToplevelSurfaceRoleAttributes>>()
                 .and_then(|attributes| attributes.lock().ok());
 
-            attributes.and_then(|attributes| attributes.current.size)
+            attributes.and_then(|attributes| attributes.last_acked.as_ref()?.state.size)
         })
         .unwrap_or_default()
     }
@@ -213,7 +213,7 @@ impl Surface for CatacombLayerSurface {
                 .get::<Mutex<LayerSurfaceAttributes>>()
                 .and_then(|attributes| attributes.lock().ok());
 
-            attributes.and_then(|attributes| attributes.current.size)
+            attributes.and_then(|attributes| attributes.last_acked.as_ref()?.state.size)
         })
         .unwrap_or_default()
     }
@@ -265,8 +265,9 @@ impl Surface for LockSurface {
     }
 
     fn acked_size(&self) -> Size<i32, Logical> {
-        let current_state = self.current_state();
-        let size = current_state.size.unwrap_or_default();
+        let size = self
+            .with_committed_state(|state| state.and_then(|state| state.size))
+            .unwrap_or_default();
         (size.w as i32, size.h as i32).into()
     }
 

commit d30574fb35f0fd877cfae1e27b96aa18dac6b9eb

Fix virtual keyboard regression
This fixes a regression in the virtual keyboard implementation
introduced in 9e88ce3, which broke all modifier support.

See Smithay/smithay#1926.

Cargo.toml
@@ -24,8 +24,8 @@ serde_json = "1.0.85"
 xkbcommon = "0.9.0"
 
 [workspace.dependencies.smithay]
-git = "https://github.com/smithay/smithay"
-rev = "0cd72d182a7f20daa13d48a14752c95130f0bef9"
+git = "https://github.com/chrisduerr/smithay"
+rev = "590e115f4e84d4982b2e901a05fea65498b36a7e"
 default-features = false
 
 [dependencies]

src/catacomb.rs
@@ -14,10 +14,8 @@ use _server_decoration::server::org_kde_kwin_server_decoration_manager::Mode as 
 use calloop::Mode;
 use catacomb_ipc::{Keysym, Orientation};
 use smithay::backend::allocator::dmabuf::Dmabuf;
-use smithay::backend::input::KeyState;
 use smithay::backend::renderer::ImportDma;
-use smithay::input::keyboard::xkb::ModMask;
-use smithay::input::keyboard::{FilterResult, KeyboardHandle, Keycode, XkbConfig};
+use smithay::input::keyboard::XkbConfig;
 use smithay::input::{Seat, SeatHandler, SeatState};
 use smithay::reexports::calloop::generic::{Generic, NoIoDrop};
 use smithay::reexports::calloop::signals::{Signal, Signals};
@@ -75,7 +73,7 @@ use smithay::wayland::single_pixel_buffer::SinglePixelBufferState;
 use smithay::wayland::socket::ListeningSocketSource;
 use smithay::wayland::text_input::TextInputManagerState;
 use smithay::wayland::viewporter::ViewporterState;
-use smithay::wayland::virtual_keyboard::{VirtualKeyboardHandler, VirtualKeyboardManagerState};
+use smithay::wayland::virtual_keyboard::VirtualKeyboardManagerState;
 use smithay::wayland::xdg_activation::{
     XdgActivationHandler, XdgActivationState, XdgActivationToken, XdgActivationTokenData,
 };
@@ -757,30 +755,7 @@ impl InputMethodHandler for Catacomb {
         self.windows.parent_geometry(parent)
     }
 }
-
-impl VirtualKeyboardHandler for Catacomb {
-    fn on_keyboard_event(
-        &mut self,
-        keycode: Keycode,
-        state: KeyState,
-        time: u32,
-        keyboard: KeyboardHandle<Self>,
-    ) {
-        let serial = SERIAL_COUNTER.next_serial();
-        keyboard.input(self, keycode, state, serial, time, |_, _, _| FilterResult::<bool>::Forward);
-    }
-
-    fn on_keyboard_modifiers(
-        &mut self,
-        _depressed_mods: ModMask,
-        _latched_mods: ModMask,
-        _locked_mods: ModMask,
-        _keyboard: KeyboardHandle<Self>,
-    ) {
-    }
-}
 delegate_virtual_keyboard_manager!(Catacomb);
-
 delegate_input_method_manager!(Catacomb);
 delegate_text_input_manager!(Catacomb);
 