catacombing/catacomb

commit f57c0cf0d28a3c30d5f70934be18c720c413285b

Move fractional scale cache to client data
This improves on 6a8e29e by changing the fractional scale to be stored
on the client data instead of the surface data.

While this technically means that applications which spawn multiple
XDG/layer shell windows with different app IDs will have less functional
per-window scaling, it also means that clients that spawn surfaces which
are not owned by a window have a chance of working. Since the latter is
much more common, this seems like a good trade-off.

src/catacomb.rs
@@ -5,6 +5,7 @@ use std::collections::HashSet;
 use std::os::fd::OwnedFd;
 use std::path::Path;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::{Duration, Instant};
 use std::{cmp, env, mem};
 
@@ -26,7 +27,7 @@ use smithay::reexports::calloop::{
 use smithay::reexports::wayland_protocols::xdg::decoration as _decoration;
 use smithay::reexports::wayland_protocols::xdg::shell::server::xdg_toplevel::WmCapabilities;
 use smithay::reexports::wayland_protocols_misc::server_decoration as _server_decoration;
-use smithay::reexports::wayland_server::backend::{ClientData, ClientId, DisconnectReason};
+use smithay::reexports::wayland_server::backend::ClientData;
 use smithay::reexports::wayland_server::protocol::wl_buffer::WlBuffer;
 use smithay::reexports::wayland_server::protocol::wl_output::WlOutput;
 use smithay::reexports::wayland_server::protocol::wl_seat::WlSeat;
@@ -852,20 +853,24 @@ impl FractionalScaleHandler for Catacomb {
         //
         // This handles setting the initial fractional scale for windows which do not
         // have the fractional scale manager initialized on window creation, by using
-        // the fractional scale that is cached on the surface on creation.
+        // the fractional scale that is cached on the client on creation, or falling
+        // back to the output's scale.
         //
-        // If this surface does not have a window and there is no cached fractional
-        // scale, we will use the output's current scale instead. This usually means
-        // these applications will not work properly with per-window scaling.
-        compositor::with_states(&surface, |states| {
-            fractional_scale::with_fractional_scale(states, |fractional_scale| {
-                if let Some(surface_data) = states.data_map.get::<RefCell<CatacombSurfaceData>>() {
-                    let cached_scale = surface_data.borrow().cached_fractional_scale;
+        // Since this uses the client data to get the cached fractional scale, it means
+        // this will only work with per-window scale if any XDG or layer shell window
+        // was created by this client before this call, or this surface is used for an
+        // XDG or layer shell window after this call. This also will not work properly
+        // for clients with multiple XDG or layer shell windows with different
+        // per-window scales.
+        if let Some(data) = surface.client().as_ref().and_then(|c| c.get_data::<ClientState>()) {
+            compositor::with_states(&surface, |states| {
+                fractional_scale::with_fractional_scale(states, |fractional_scale| {
+                    let cached_scale = data.cached_fractional_scale();
                     let scale = cached_scale.unwrap_or(self.windows.canvas().scale());
                     fractional_scale.set_preferred_scale(scale);
-                }
+                });
             });
-        });
+        }
     }
 }
 delegate_fractional_scale!(Catacomb);
@@ -969,15 +974,27 @@ delegate_viewporter!(Catacomb);
 
 /// Unused state to make Smithay happy.
 #[derive(Default)]
-struct ClientState {
+pub struct ClientState {
+    pub cached_fractional_scale: AtomicU64,
+
     compositor_state: CompositorClientState,
 }
 
-impl ClientData for ClientState {
-    fn initialized(&self, _client_id: ClientId) {}
+impl ClientState {
+    /// Get cached per-window fractional scale.
+    pub fn cached_fractional_scale(&self) -> Option<f64> {
+        let bits = self.cached_fractional_scale.load(Ordering::Relaxed);
+        (bits != u64::MAX).then(|| f64::from_bits(bits))
+    }
 
-    fn disconnected(&self, _client_id: ClientId, _reason: DisconnectReason) {}
+    /// Update per-window fractional scale cache.
+    pub fn set_cached_fractional_scale(&self, scale: f64) {
+        let bits = scale.to_bits();
+        self.cached_fractional_scale.store(bits, Ordering::Relaxed);
+    }
 }
+
+impl ClientData for ClientState {}
 
 delegate_single_pixel_buffer!(Catacomb);
 

src/drawing.rs
@@ -541,7 +541,6 @@ impl Graphics {
 /// Surface data store.
 pub struct CatacombSurfaceData {
     pub opaque_region: Vec<(RectangleKind, Rectangle<i32, Logical>)>,
-    pub cached_fractional_scale: Option<f64>,
     pub texture: Option<RenderTexture>,
     pub buffer_size: Size<i32, Logical>,
     pub src_rect: Rectangle<f64, Logical>,
@@ -560,7 +559,6 @@ impl Default for CatacombSurfaceData {
     fn default() -> Self {
         Self {
             scale: 1,
-            cached_fractional_scale: Default::default(),
             inhibits_shortcuts: Default::default(),
             opaque_region: Default::default(),
             buffer_size: Default::default(),

src/windows/window.rs
@@ -35,6 +35,7 @@ use smithay::wayland::shell::xdg::{
 use smithay::wayland::{fractional_scale, single_pixel_buffer};
 use tracing::error;
 
+use crate::catacomb::ClientState;
 use crate::drawing::{CatacombElement, CatacombSurfaceData, RenderTexture, Texture};
 use crate::geometry::Vector;
 use crate::output::{ExclusiveSpace, Output};
@@ -618,13 +619,13 @@ impl<S: Surface + 'static> Window<S> {
         let scale = self.window_scale(output_scale);
         let surface = self.surface.surface();
 
+        // Cache scale on client in case fractional scale isn't initialized yet.
+        if let Some(data) = surface.client().as_ref().and_then(|c| c.get_data::<ClientState>()) {
+            data.set_cached_fractional_scale(scale);
+        }
+
         // Update fractional scale protocol.
         compositor::with_states(surface, |states| {
-            // Cache scale on surface in case fractional scale isn't initialized yet.
-            let surface_data =
-                states.data_map.get_or_insert(|| RefCell::new(CatacombSurfaceData::new()));
-            surface_data.borrow_mut().cached_fractional_scale = Some(scale);
-
             // Submit through fractional scaling protocol if it is initialized.
             fractional_scale::with_fractional_scale(states, |fractional_scale| {
                 fractional_scale.set_preferred_scale(scale);

commit 595acc0997bbde75783cdfbea3619cb0399a12ab

Fix default cached fractional scale
This fixes an issue introduced in f57c0cf, where the fractional scale
was using the `u64::MAX` bits to detect an empty cached scale, but
initializing it using `0` as empty.

Since `0` is not a valid scale, we can just keep the existing
initialization, but instead of treating `u64::MAX` as empty we now use
`0` as empty instead.

src/catacomb.rs
@@ -1139,7 +1139,7 @@ impl ClientState {
     /// Get cached per-window fractional scale.
     pub fn cached_fractional_scale(&self) -> Option<f64> {
         let bits = self.cached_fractional_scale.load(Ordering::Relaxed);
-        (bits != u64::MAX).then(|| f64::from_bits(bits))
+        (bits != 0).then(|| f64::from_bits(bits))
     }
 
     /// Update per-window fractional scale cache.